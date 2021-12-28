157e5db0e7985d6939b874166b66fd118425420e
林深时觉寒 <61862961+ikangjia@users.noreply.github.com>
2021-12-28 16:07:27 +0800

    update WeChat id

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,4 +1,4 @@
 console.log('Ahhhhhh~');
 console.log('Congratulations you found me!');
 console.log('If you want contact me by Wechat:');
-console.log("atob('a2FuZ2ppYTEzMjQ=')");
+console.log("atob('aWthbmdqaWEtY24=')");
