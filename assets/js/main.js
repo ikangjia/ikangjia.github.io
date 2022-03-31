96f4f7045586b1fcf6e3bd2f4f85bc636016ed8d
林深时觉寒 <61862961+ikangjia@users.noreply.github.com>
2022-03-31 12:14:06 +0800

    修改背景图片

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,8 +42,8 @@ $(document).ready(function () {
 	 * 获取Bing壁纸
 	 */
 	var url = "https://api.sunweihu.com/api/bing1/api.php";
-	$('#panel').css("background", "url('"+url+"') center center no-repeat #666");
-	$('#panel').css("background-size", "cover !important");
+	$('#panel').css("background", "url('"+url+"') center center cover no-repeat #666 !important");
+// 	$('#panel').css("background-size", "cover !important");
 	$(".iUp").each(function (i, e) {
 		iUp.up(e);
 	});
